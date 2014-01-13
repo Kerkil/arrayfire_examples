814559bd56ae7e3ee0150c22afe7646d854d3bb1
shehzan <mzshehzanayub@gmail.com>
2014-01-13 17:40:02 -0500

    Added files for generation of visual studio solutions

---
+++ vs-files/update.bat
@@ -0,0 +1,42 @@
+REM REM is comments
+REM USAGE: From ArrayFire-Examples folder:
+REM ArrayFire-Examples> .\vs-files\update.bat ..\ArrayFire-Examples
+
+@echo off
+
+IF EXIST %~f1 (
+    echo Directory Found
+)
+IF NOT EXIST %~f1 (
+    echo Directory Not Found
+    exit
+)
+
+REM List full path
+REM for /R %%f in (*.cpp) do echo %%f
+REM List only filename without extension
+REM for /R %%f in (*.cpp) do echo %%~nf
+REM List only filename with extension
+REM for /R %%f in (*.cpp) do echo %%~nxf
+REM List only relative
+REM for /R %%f in (*.cpp) do echo %%~pf
+
+set VSDIR=%~dp0
+set SEARCH=XXX
+for /R %%f in (*.cpp) do (
+    echo %%f
+    PUSHD %%~pf
+    copy /y %VSDIR%vs2012.sln .\%%~nf_vs2012.sln
+    copy /y %VSDIR%vs2010.sln .\%%~nf_vs2010.sln
+    copy /y %VSDIR%vs2008.sln .\%%~nf_vs2008.sln
+    copy /y %VSDIR%vs2012.vcxproj .\%%~nf_vs2012.vcxproj
+    copy /y %VSDIR%vs2010.vcxproj .\%%~nf_vs2010.vcxproj
+    copy /y %VSDIR%vs2008.vcproj .\%%~nf_vs2008.vcproj
+    %VSDIR%\fnr.exe %SEARCH% %%~nf .\%%~nf_vs2012.sln
+    %VSDIR%\fnr.exe %SEARCH% %%~nf .\%%~nf_vs2010.sln
+    %VSDIR%\fnr.exe %SEARCH% %%~nf .\%%~nf_vs2008.sln
+    %VSDIR%\fnr.exe %SEARCH% %%~nf .\%%~nf_vs2012.vcxproj
+    %VSDIR%\fnr.exe %SEARCH% %%~nf .\%%~nf_vs2010.vcxproj
+    %VSDIR%\fnr.exe %SEARCH% %%~nf .\%%~nf_vs2008.vcproj
+    POPD
+)
