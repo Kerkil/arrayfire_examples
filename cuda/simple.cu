a0194e8b3e46ef96805c61489b0b2adda5f6a71c
pavan <pavan@arrayfire.com>
2014-05-23 15:29:04 -0400

    Cleaning up the simple CUDA example
    - Includes the necessary unlocks

--- cuda/simple.cu
+++ cuda/simple.cu
@@ -9,11 +9,12 @@ int main()
     // Generate input data
     af::array x = af::randu(num, 1);
 
+    // Create empty array with required dimensions
+    af::array y = af::array(num, 1);
+
     // Get device pointers
     float *d_x = x.device<float>();
-
-    // Allocate data needed for output
-    float *d_y = af::array::alloc<float>(num);
+    float *d_y = y.device<float>();
 
     // Finish the tasks arrayfire was doing
     af::sync();
@@ -22,13 +23,15 @@ int main()
     // y = sin(x)^2 + con(x)^2
     launch_simple_kernel(d_y, d_x, num);
 
-    // Create arrays from output data
-    af::array y(num, d_y);
+    // Tell arrayfire it controls the pointers again
+    x.unlock();
+    y.unlock();
 
     // Check for errors
     // sin(x)^ + cos(x)^2 == 1
     // The following should print 0
     float err = af::sum<float>(y - 1);
     printf("Error: %f\n", err);
+
     return 0;
 }
